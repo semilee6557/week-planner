d16d6f167418da473e1ec3c0aa1b6da7f1ae37d5
semi lee <semilee6557@gmail.com>
2020-10-17 12:31:02 -0700

    complete user-can-add-entry-to-day

--- main.js
+++ main.js
@@ -5,75 +5,119 @@
 // }
 
 var schedule = {
-  Sunday: {},
-  Monday: {},
-  Tuesday: {},
-  Wednesday: {},
-  Thursday: {},
-  Friday: {},
-  Saturday: {}
-}
+  Sunday: {
+    day: 'Sunday',
+    isSelected: false,
+    todo: {
+    }
+  },
+  Monday: {
+    day: 'Monday',
+    isSelected: false,
+    todo: {
+    }
+
+
+  },
+  Tuesday: {
+    day: 'Tuesday',
+    isSelected: false,
+    todo: {
+    }
+
+
+  },
+  Wednesday: {
+    day: 'Wednesday',
+    isSelected: false,
+    todo: {
+    }
+
+
+  },
+  Thursday: {
+    day: 'Thursday',
+    isSelected: false,
+    todo: {
+    }
+
+
+  },
+  Friday: {
+    day: 'Friday',
+    isSelected: false,
+    todo: {
+    }
+
+
+  },
+  Saturday: {
+    day: 'Saturday',
+    isSelected: false,
+    todo: {
+    }
+
+
+  }
+};
+
 
 
 var days = document.querySelector('.days');
-var h2 = document.querySelector('h2');
-var selectedText = '';
-var addEntryBtn = document.querySelector('.add-entry')
-var modal = document.querySelector('.modal')
-var scheduleDay = document.querySelector('#day')
-var scheduleTime = document.querySelector('#time')
-var dayOption = document.querySelectorAll('.day-option')
-var form = document.querySelector('form')
-var submit = document.querySelector('#submit')
+var modal = document.querySelector('.modal');
+var addEntryBtn = document.querySelector('.add-entry');
+var form = document.querySelector('form');
+
+
+var scheduleDay = document.querySelector('#day');
+var scheduleTime = document.querySelector('#time');
 var textarea = document.querySelector('textarea');
-var dayNum = ''
-var timeNum = ''
-var willscheduleDay = ''
-var selectedTime = ''
-var tr = document.querySelectorAll('tr')
-var td = document.querySelectorAll('td')
 
-days.addEventListener('click', function () {
-  selectedDay(event)
-});
-addEntryBtn.addEventListener('click', scheduleEvent);
-form.addEventListener('change', selectedSchedule);
-submit.addEventListener('click', update);
 
-function selectedDay(event) {
-  if (event.target === days) {
-    return
+days.addEventListener('click', selectedDay);
+addEntryBtn.addEventListener('click', showModal);
+form.addEventListener('change', setSchedule);
+
+
+function selectedDay() {
+  if (event.target.className === days.className) {
+    return;
   } else {
-    selectedText = event.target.textContent;
+    var selectedText = event.target.textContent;
+    var h2 = document.querySelector('h2');
+
     h2.textContent = 'Scheduled Events for ' + selectedText;
+
+      var td = document.querySelectorAll('td');
+
+
+  td[0].textContent = schedule[selectedText].todo.time;
+  td[1].textContent = schedule[selectedText].todo.description;
+
   }
 }
 
-function scheduleEvent() {
-  modal.className = "modal"
-  console.log(modal.className)
+function showModal() {
+  modal.className = "modal";
 }
 
+function setSchedule() {
 
+  var dayNum = scheduleDay.selectedIndex;
+  var timeNum = scheduleTime.selectedIndex;
+  var willScheduleDay = scheduleDay.options[dayNum].textContent;
+  var willScheduleTime = scheduleTime.options[timeNum].textContent;
+  var description = textarea.textContent;
 
+  schedule[willScheduleDay].todo.time = willScheduleTime;
+  schedule[willScheduleDay].todo.description = description;
+  var submit = document.querySelector('#submit');
 
-function selectedSchedule() {
+submit.addEventListener('click', update);
 
-  dayNum = scheduleDay.selectedIndex;
-  timeNum = scheduleTime.selectedIndex;
-  willscheduleDay = scheduleDay.options[dayNum].textContent;
-  selectedTime = scheduleTime.options[timeNum].textContent;
-  description = textarea.textContent
-  console.log(willscheduleDay, selectedTime, description)
 }
-
 
 function update() {
-  modal.className += " hidden"
-  // schedule[willscheduleDay][selectedTime]=description;
-  // console.log(willscheduleDay, selectedTime, description)
-  // console.log(schedule[willscheduleDay])
-td[0].textContent=selectedTime
-td[1].textContent=description
+  modal.className += " hidden";
 
 }
