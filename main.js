7ff2c43207c5880d8d04476ed521b685980c68fb
semi lee <semilee6557@gmail.com>
2020-10-17 10:41:20 -0700

    fixed code

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 var days = document.querySelector('.days');
 var h2 = document.querySelector('h2');
-var selectedDay = '';
 
-days.addEventListener('click', function (event) {
-  if (event.target===days){
-    return
+days.addEventListener('click', selectedDay);
+
+function selectedDay() {
+  if (event.target.className === days.className) {
+    return;
   } else {
-    selectedDay = event.target.textContent;
-    h2.textContent = 'Scheduled Events for ' + selectedDay;
+    var selectedText = event.target.textContent;
+    h2.textContent = 'Scheduled Events for ' + selectedText;
   }
-});
+}
+
+// days.addEventListener('click', function (event) {
+//   if (event.target===days){
+//     return
+//   } else {
+//     selectedDay = event.target.textContent;
+//     h2.textContent = 'Scheduled Events for ' + selectedDay;
+//   }
+// });
