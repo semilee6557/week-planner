1ac136ba54c51bf488c70c21bc532d287d9c01fe
semilee6557 <61755795+semilee6557@users.noreply.github.com>
2020-10-17 16:14:22 -0700

    Revert "User can add entry to day"

--- main.js
+++ main.js
@@ -1,58 +1,22 @@
-// class weekSchedule {
-//   constructor{
-// this.day = {}
-//   }
-// }
-
-var schedule = {
-  Sunday: {
-    day: 'Sunday',
-    todo: []
-  },
-  Monday: {
-    day: 'Monday',
-    todo: []
-
-
-  },
-  Tuesday: {
-    day: 'Tuesday',
-    todo: []
-
-
-  },
-  Wednesday: {
-    day: 'Wednesday',
-    todo: []
-
-
-  },
-  Thursday: {
-    day: 'Thursday',
-    todo: []
-
-
-  },
-  Friday: {
-    day: 'Friday',
-    todo: []
-
-
-  },
-  Saturday: {
-    day: 'Saturday',
-    todo: []
-
-
-  }
-};
-
-
-
 var days = document.querySelector('.days');
+var h2 = document.querySelector('h2');
+
+days.addEventListener('click', selectedDay);
 
 function selectedDay() {
   if (event.target.className === days.className) {
     return;
   } else {
+    var selectedText = event.target.textContent;
+    h2.textContent = 'Scheduled Events for ' + selectedText;
+  }
+}
 
+// days.addEventListener('click', function (event) {
+//   if (event.target===days){
+//     return
+//   } else {
+//     selectedDay = event.target.textContent;
+//     h2.textContent = 'Scheduled Events for ' + selectedDay;
+//   }
+// });
